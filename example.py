2f515265fb7f7ef0fe97766eee07d1d22673e7b8
PramodSeeram <seerampramodsailesh@gmail.com>
2024-06-14 15:41:48 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,14 +1,1 @@
-def git_opeation():
-
- print("I am adding example.py file to the remote repository.")
- print("test")
- print("test1")
-git_opeation()
-;,MF'LBFKN3MF
-/'.;LMKNJ LKNM;,'
-FW';LFK JLKNM;L,./?
-W3J FNMW,;'./
-
-
-
-DJLKNM;'.KOBJVHI GUCTIYFGULKJH;K,L./
+;'.KOBJVHI GUCTIYFGULKJH;K,L./
